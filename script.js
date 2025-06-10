b5607741f91032411787d77ae6f9f5e02140cd71
Osteenjr <osteenonyeke@gmail.com>
2025-06-10 11:27:26 +0100

    Made some changes, made it responsive on smaller screens and added functionality for smaller screens. I also added buttons that link to my github, mail, linkedIn, X and my cv

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'none';
-}
+            const sidebar = document.getElementById('sidebar');
+            sidebar.classList.remove('active');
+        }
 
-function showSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'flex';
-}
+        function showSidebar() {
+            const sidebar = document.getElementById('sidebar');
+            sidebar.classList.add('active');
+        }
+
+        
+        document.addEventListener('click', function(event) {
+            const sidebar = document.getElementById('sidebar');
+            const menuBtn = document.querySelector('.menu-btn');
+            
+            if (!sidebar.contains(event.target) && !menuBtn.contains(event.target)) {
+                hideSidebar();
+            }
+        });
+
+       
+        window.addEventListener('resize', function() {
+            if (window.innerWidth > 768) {
+                hideSidebar();
+            }
+});
